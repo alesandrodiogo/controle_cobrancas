39ae824904d7ce6d7c7e31efe00cb4f516a1f768
Alesandro <alesandromendesdiogo24@gmail.com>
2023-05-18 15:54:25 +0100

    cliente commit
    CRUD

--- ControleCobrancaADM/ControleCobrancaADM/Controllers/ClienteController.cs
+++ ControleCobrancaADM/ControleCobrancaADM/Controllers/ClienteController.cs
@@ -1,5 +1,6 @@
 ﻿using ControleCobrancaADM.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ControleCobrancaADM.Controllers
 {
@@ -10,15 +11,49 @@ namespace ControleCobrancaADM.Controllers
         {
             if (_clientes.Count.Equals(0))
             {
-                _clientes.Add(new Clientes() { Nome = "Pericles Jaime", Idade = 25, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro" }); ;
-                _clientes.Add(new Clientes() { Nome = "Alesandro Diogo", Idade = 23, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro" });
-                _clientes.Add(new Clientes() { Nome = "Edgar Domingos", Idade = 27, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro", });
+                _clientes.Add(new Clientes() { Id = 1, Nome = "Pericles Jaime", Idade = 25, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro" }); ;
+                _clientes.Add(new Clientes() { Id = 2, Nome = "Alesandro Diogo", Idade = 23, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro" });
+                _clientes.Add(new Clientes() { Id = 3, Nome = "Edgar Domingos", Idade = 27, Pais = "Angola", Morada = "Luanda/Angola", DataNascimento = DateTime.Now, EstadoCivil = "Solteiro", });
             }
-
+             
         }
         public IActionResult Listar()
         {
             return View(_clientes);
         }
+        public IActionResult Edit(int id)
+        {
+            var cliente = _clientes.FirstOrDefault(x => x.Id.Equals(id));
+            return View(cliente);
+        }
+        [HttpPost]
+        public IActionResult Edit([FromForm] Clientes cliente)
+        {
+            var clienteSelecionado = _clientes.FirstOrDefault(x => x.Id.Equals(cliente.Id));
+            clienteSelecionado.Nome = cliente.Nome;
+            clienteSelecionado.Idade = cliente.Idade;
+            clienteSelecionado.Pais = cliente.Pais;
+            clienteSelecionado.Morada = cliente.Morada;
+            clienteSelecionado.DataNascimento = cliente.DataNascimento;
+            clienteSelecionado.EstadoCivil = cliente.EstadoCivil;
+            return RedirectToAction("Listar");
+        }
+
+        public IActionResult Novo() 
+        { 
+            return View(new Clientes());
+        }
+        [HttpPost]
+        public IActionResult Novo([FromForm] Clientes clienteNovo)
+        {
+            _clientes.Add(clienteNovo);
+            return RedirectToAction("Listar");
+        }
+        /*[HttpDelete]
+        public IActionResult Deletar([FromForm] Clientes cliente, int id)
+        {
+            _clientes.RemoveAt(cliente.Id);
+            return View("Listar");
+        }*/
     }
 }
--- ControleCobrancaADM/ControleCobrancaADM/Models/Clientes.cs
+++ ControleCobrancaADM/ControleCobrancaADM/Models/Clientes.cs
@@ -5,6 +5,7 @@ namespace ControleCobrancaADM.Models
     public class Clientes
     {
         [Required]
+        public int Id { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Pais { get; set; }
